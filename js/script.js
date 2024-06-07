a56585ba29f2c8c8374e28bd2869b9e8434cb3d9
Azhar Khan <119923339+azharkhan93@users.noreply.github.com>
2024-06-07 17:04:45 +0530

    hello

--- js/script.js
+++ js/script.js
@@ -69,33 +69,35 @@
 		$window.on( 'scroll', scrollHandler );
 	}
 
-	// Initialize scripts that require a loaded page
 	$window.on('load', function () {
-		// Page loader & Page transition
-		if (plugins.preloader.length && !isNoviBuilder) {
-			pageTransition({
-				target: document.querySelector('.page'),
-				delay: 0,
-				duration: pageTransitionAnimationDuration,
-				classActive: 'animated',
-				conditions: function (event, link) {
-					return !/(\#|callto:|tel:|mailto:|:\/\/)/.test(link)
-							&& !event.currentTarget.hasAttribute('data-lightgallery')
-							&& event.currentTarget.getAttribute('href') !== 'javascript:void(0);';
-				},
-				onTransitionStart: function (options) {
-					setTimeout(function () {
-						plugins.preloader.removeClass('loaded');
-					}, options.duration * .75);
-				},
-				onReady: function () {
-					plugins.preloader.addClass('loaded');
-					windowReady = true;
-				}
-			});
-		}
-	});
-
+    // Page loader & Page transition
+    if (plugins.preloader.length && !isNoviBuilder) {
+        // Immediately hide the preloader to avoid it getting stuck
+        plugins.preloader.addClass('loaded');
+        
+        // Optional: you can still initialize the page transition if needed
+        pageTransition({
+            target: document.querySelector('.page'),
+            delay: 0,
+            duration: pageTransitionAnimationDuration,
+            classActive: 'animated',
+            conditions: function (event, link) {
+                return !/(\#|callto:|tel:|mailto:|:\/\/)/.test(link)
+                        && !event.currentTarget.hasAttribute('data-lightgallery')
+                        && event.currentTarget.getAttribute('href') !== 'javascript:void(0);';
+            },
+            onTransitionStart: function (options) {
+                setTimeout(function () {
+                    plugins.preloader.removeClass('loaded');
+                }, options.duration * .75);
+            },
+            onReady: function () {
+                plugins.preloader.addClass('loaded');
+                windowReady = true;
+            }
+        });
+    }
+});
 	// Initialize scripts that require a finished document
 	$(function () {
 		isNoviBuilder = window.xMode;
@@ -891,95 +893,7 @@
 			attachFormValidator(plugins.regula);
 		}
 
-		// MailChimp Ajax subscription
-		// if (plugins.mailchimp.length) {
-		// 	for (i = 0; i < plugins.mailchimp.length; i++) {
-		// 		var $mailchimpItem = $(plugins.mailchimp[i]),
-		// 				$email = $mailchimpItem.find('input[type="email"]');
-
-				
-		// 		$mailchimpItem.attr('novalidate', 'true');
-		// 		$email.attr('name', 'EMAIL');
-
-		// 		$mailchimpItem.on('submit', $.proxy(function ($email, event) {
-		// 			event.preventDefault();
-
-		// 			var $this = this;
-
-		// 			var data = {},
-		// 					url = $this.attr('action').replace('/post?', '/post-json?').concat('&c=?'),
-		// 					dataArray = $this.serializeArray(),
-		// 					$output = $("#" + $this.attr("data-form-output"));
-
-		// 			for (i = 0; i < dataArray.length; i++) {
-		// 				data[dataArray[i].name] = dataArray[i].value;
-		// 			}
-
-		// 			$.ajax({
-		// 				data: data,
-		// 				url: url,
-		// 				dataType: 'jsonp',
-		// 				error: function (resp, text) {
-		// 					$output.html('Server error: ' + text);
-
-		// 					setTimeout(function () {
-		// 						$output.removeClass("active");
-		// 					}, 4000);
-		// 				},
-		// 				success: function (resp) {
-		// 					$output.html(resp.msg).addClass('active');
-		// 					$email[0].value = '';
-		// 					var $label = $('[for="' + $email.attr('id') + '"]');
-		// 					if ($label.length) $label.removeClass('focus not-empty');
-
-		// 					setTimeout(function () {
-		// 						$output.removeClass("active");
-		// 					}, 6000);
-		// 				},
-		// 				beforeSend: function (data) {
-		// 					var isNoviBuilder = window.xMode;
-
-		// 					var isValidated = (function () {
-		// 						var results, errors = 0;
-		// 						var elements = $this.find('[data-constraints]');
-		// 						var captcha = null;
-		// 						if (elements.length) {
-		// 							for (var j = 0; j < elements.length; j++) {
-
-		// 								var $input = $(elements[j]);
-		// 								if ((results = $input.regula('validate')).length) {
-		// 									for (var k = 0; k < results.length; k++) {
-		// 										errors++;
-		// 										$input.siblings(".form-validation").text(results[k].message).parent().addClass("has-error");
-		// 									}
-		// 								} else {
-		// 									$input.siblings(".form-validation").text("").parent().removeClass("has-error")
-		// 								}
-		// 							}
-
-		// 							if (captcha) {
-		// 								if (captcha.length) {
-		// 									return validateReCaptcha(captcha) && errors === 0
-		// 								}
-		// 							}
-
-		// 							return errors === 0;
-		// 						}
-		// 						return true;
-		// 					})();
-
-							
-		// 					if (isNoviBuilder || !isValidated)
-		// 						return false;
-
-		// 					$output.html('Submitting...').addClass('active');
-		// 				}
-		// 			});
-
-		// 			return false;
-		// 		}, $mailchimpItem, $email));
-		// 	}
-		// }
+		
 
 		// Campaign Monitor ajax subscription
 		if (plugins.campaignMonitor.length) {
@@ -1037,154 +951,7 @@
 			}
 		}
 
-		// RD Mailform
-		// if (plugins.rdMailForm.length) {
-		// 	var i, j, k,
-		// 			msg = {
-		// 				'MF000': 'Successfully sent!',
-		// 				'MF001': 'Recipients are not set!',
-		// 				'MF002': 'Form will not work locally!',
-		// 				'MF003': 'Please, define email field in your form!',
-		// 				'MF004': 'Please, define type of your form!',
-		// 				'MF254': 'Something went wrong with PHPMailer!',
-		// 				'MF255': 'Aw, snap! Something went wrong.'
-		// 			};
-
-		// 	for (i = 0; i < plugins.rdMailForm.length; i++) {
-		// 		var $form = $(plugins.rdMailForm[i]),
-		// 				formHasCaptcha = false;
-
-		// 		$form.attr('novalidate', 'novalidate').ajaxForm({
-		// 			data: {
-		// 				"form-type": $form.attr("data-form-type") || "contact",
-		// 				"counter": i
-		// 			},
-		// 			beforeSubmit: function (arr, $form, options) {
-		// 				if (isNoviBuilder)
-		// 					return;
-
-		// 				var form = $(plugins.rdMailForm[this.extraData.counter]),
-		// 						inputs = form.find("[data-constraints]"),
-		// 						output = $("#" + form.attr("data-form-output")),
-		// 						captcha = form.find('.recaptcha'),
-		// 						captchaFlag = true;
-
-		// 				output.removeClass("active error success");
-
-		// 				if (isValidated(inputs, captcha)) {
-
-		// 					// veify reCaptcha
-		// 					if (captcha.length) {
-		// 						var captchaToken = captcha.find('.g-recaptcha-response').val(),
-		// 								captchaMsg = {
-		// 									'CPT001': 'Please, setup you "site key" and "secret key" of reCaptcha',
-		// 									'CPT002': 'Something wrong with google reCaptcha'
-		// 								};
-
-		// 						formHasCaptcha = true;
-
-		// 						$.ajax({
-		// 							method: "POST",
-		// 							url: "bat/reCaptcha.php",
-		// 							data: {'g-recaptcha-response': captchaToken},
-		// 							async: false
-		// 						})
-		// 						.done(function (responceCode) {
-		// 							if (responceCode !== 'CPT000') {
-		// 								if (output.hasClass("snackbars")) {
-		// 									output.html('<p><span class="icon text-middle mdi mdi-check icon-xxs"></span><span>' + captchaMsg[responceCode] + '</span></p>')
-
-		// 									setTimeout(function () {
-		// 										output.removeClass("active");
-		// 									}, 3500);
-
-		// 									captchaFlag = false;
-		// 								} else {
-		// 									output.html(captchaMsg[responceCode]);
-		// 								}
-
-		// 								output.addClass("active");
-		// 							}
-		// 						});
-		// 					}
-
-		// 					if (!captchaFlag) {
-		// 						return false;
-		// 					}
-
-		// 					form.addClass('form-in-process');
-
-		// 					if (output.hasClass("snackbars")) {
-		// 						output.html('<p><span class="icon text-middle fa fa-circle-o-notch fa-spin icon-xxs"></span><span>Sending</span></p>');
-		// 						output.addClass("active");
-		// 					}
-		// 				} else {
-		// 					return false;
-		// 				}
-		// 			},
-		// 			error: function (result) {
-		// 				if (isNoviBuilder)
-		// 					return;
-
-		// 				var output = $("#" + $(plugins.rdMailForm[this.extraData.counter]).attr("data-form-output")),
-		// 						form = $(plugins.rdMailForm[this.extraData.counter]);
-
-		// 				output.text(msg[result]);
-		// 				form.removeClass('form-in-process');
-
-		// 				if (formHasCaptcha) {
-		// 					grecaptcha.reset();
-		// 				}
-		// 			},
-		// 			success: function (result) {
-		// 				if (isNoviBuilder)
-		// 					return;
-
-		// 				var form = $(plugins.rdMailForm[this.extraData.counter]),
-		// 						output = $("#" + form.attr("data-form-output")),
-		// 						select = form.find('select');
-
-		// 				form
-		// 				.addClass('success')
-		// 				.removeClass('form-in-process');
-
-		// 				if (formHasCaptcha) {
-		// 					grecaptcha.reset();
-		// 				}
-
-		// 				result = result.length === 5 ? result : 'MF255';
-		// 				output.text(msg[result]);
-
-		// 				if (result === "MF000") {
-		// 					if (output.hasClass("snackbars")) {
-		// 						output.html('<p><span class="icon text-middle mdi mdi-check icon-xxs"></span><span>' + msg[result] + '</span></p>');
-		// 					} else {
-		// 						output.addClass("active success");
-		// 					}
-		// 				} else {
-		// 					if (output.hasClass("snackbars")) {
-		// 						output.html(' <p class="snackbars-left"><span class="icon icon-xxs mdi mdi-alert-outline text-middle"></span><span>' + msg[result] + '</span></p>');
-		// 					} else {
-		// 						output.addClass("active error");
-		// 					}
-		// 				}
-
-		// 				form.clearForm();
-
-		// 				if (select.length) {
-		// 					select.select2("val", "");
-		// 				}
-
-		// 				form.find('input, textarea').trigger('blur');
-
-		// 				setTimeout(function () {
-		// 					output.removeClass("active error success");
-		// 					form.removeClass('success');
-		// 				}, 3500);
-		// 			}
-		// 		});
-		// 	}
-		// }
+		
 
 		// lightGallery
 		if (plugins.lightGallery.length) {
